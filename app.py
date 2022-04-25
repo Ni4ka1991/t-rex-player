e359b0ccccbcbbd870f9aafb744fa96e14c27be7
Ni4ka1991 <astahova_nica@mail.ru>
2022-04-25 15:25:15 +0300

    Tested. Add model.

--- app.py
+++ app.py
@@ -4,76 +4,34 @@
 import torch
 from torch import nn
 from torchvision.transforms.functional import rgb_to_grayscale, invert, normalize
+from torch.nn.functional import normalize
 from torchvision.io import read_image
 from PIL import Image, ImageOps
+from os import system
+
 
 #PLOT
 import matplotlib.pyplot as plt
 
 
 #LOAD IMAGE AS TENSOR
+system( "clear" )
 
-img = read_image( "images/t-rex-up-1.jpg" )  #read image as tensor???
-print( img )
-print( img.shape )
+img           = read_image( "images/t-rex-up-1.jpg" )  #read image as tensor!!!
+img_gray      = rgb_to_grayscale( img )
+img_invert    = invert(img_gray)
+img_float     = img_invert.type(torch.float32)
+img_normalize = normalize( img_float )
+img_unsq      = img_normalize.unsqueeze_(0)
 
-img_gray = rgb_to_grayscale( img )
-print( img_gray )
-print( img_gray.shape )
+model = nn.Sequential(
+        nn.Conv2d( 1, 1, (40,40))
+)
 
-img_invert = invert(img_gray)
-print( img_invert )
-print( img_invert.shape )
-
-
-cnn = nn.Conv2d( 1, 1, 7 )
-img_filtered = cnn( img_invert )
-print( img_filtered )
-print( img_filtered.shape )
-
-
-
-
-
+y = model( img_unsq )
+print(y.shape)
+img_sqz = y.squeeze(0)
+plt.imshow( img_sqz.detach().permute( 1, 2, 0 ), interpolation='nearest', cmap = 'gray' )
+plt.show()
 '''
-plt.imshow( img, interpolation = 'nearest', cmap='gray' )
-plt.show()
-img_gray = rgb_to_grayscale(img)
-#plt.imshow( tensor.permute( 1, 2, 0 ), cmap='gray' )
-#plt.show()
-
-
-tensor = invert( rgb_to_grayscale(
-        read_image( "images/t-rex-up-1.jpg" )
-)).type(torch.float32)
-
-print( tensor )
-print( tensor.shape )
-tensor2 = tensor.unsqueeze_(0)
-
-
-#plt.imshow( tensor.permute( 1, 2, 0 ), cmap='gray' )
-#plt.show()
-
-
-cnn = nn.Conv2d( 1, 1, 40 )
-
-filtered_img = cnn( tensor2 )
-
-plt.figure()
-
-plt.imshow( filtered_img.detach().permute( 1, 2, 0 ), interpolation='nearest', cmap = 'gray' )
-plt.show()
-
-
-
-
-
-#model = nn.Sequential(
-#        nn.Conv2d( 1, 1, 40 ),
-#)
-
-#y = model( tensor )
-#plt.imshow( y.detach().permute( 1, 2, 0 ), cmap='gray' )
-#plt.show()
 '''
