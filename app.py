9e4158c2f7d6eb4905700124973bd8e769414deb
root <root@Veronika-HP.localdomain>
2022-04-30 09:13:48 +0300

    Some changes

--- app.py
+++ app.py
@@ -17,6 +17,11 @@ import matplotlib.pyplot as plt
 #LOAD IMAGE AS TENSOR
 system( "clear" )
 
+def view_img( img ):
+    img_sqz = img.squeeze(0)
+    plt.imshow( img_sqz.detach().permute( 1, 2, 0 ), interpolation='nearest', cmap = 'gray' )
+    plt.show()
+
 def loadImageAsTensor( path ):
     tensor = ( normalize( invert( rgb_to_grayscale(
         read_image( path ))).type(torch.float32))
@@ -28,17 +33,21 @@ def loadImageAsTensor( path ):
 tensor_cactus  = loadImageAsTensor( "images/two-1.jpg" )
 weights_cactus = loadImageAsTensor( "images/cactus-weights.jpg" )
 
+#view_img( tensor_cactus )
+#view_img( weights_cactus )
 
 def detectImminentThreat( tensor, weights ):
     model = nn.Sequential(
             nn.Conv2d( 1, 1, ( 46, 46 )),
-            nn.AvgPool2d(( 1, 19 )),
+            nn.MaxPool2d(( 19, 1 )),
     )
 
     model[0].weight = nn.Parameter( weights ) 
     y = model( tensor )
-
-
+    y_pos = torch.argmax( y )
+#    print( f"y.shape >>> {y.shape}" )
+#    view_img( y )
+    return y_pos    
 
 
 
