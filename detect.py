e5d148a0b71d08db916cbeeba83a67975659581e
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-06-11 11:20:47 +0300

    Some changes

--- detect.py
+++ detect.py
@@ -14,22 +14,25 @@ import numpy as np
 class detectSomething():
 
     
-    def getFilteredImg( self, arr_dim, weights, tensor ):
+    def getMaxpooledImg( self, arr_dim, weights, tensor, "maxpool_axis" ):
+         
+         model = nn.Sequential(
+                nn.Conv2d( 1, 1, ( arr_dim, arr_dim )),
+                nn.MaxPool2d(( a, b )),
+        )
         
-        ##convolution the image 
-        filteredImg = nn.Conv2d( 1, 1, ( arr_dim, arr_dim ))      #( 1 -> grey scale input, 1 -> grey scale output, arr_dim -> filter dimentions ) 
-        filteredImg.weight = nn.Parameter( weights )              #use predefined filter names "weights_vertical" in this case
-        y_filtered = filteredImg( tensor )                        # >>> y 
+        model[0].weight = nn.Parameter( weights ) 
         
-        return y_filtered
-
+        if "maxpool_axis" == "v":
+            a = 1 and b = shape[3]
+        elif "maxpool_axis" == "h":
+            a = shape[3] and b = 1
+        
+        y = model( tensor )
+        return y
     
     def getCoordinates( self, y ):
         
-        shape = y.shape
-
-        ##average pooling convolution
-        maxImg = nn.MaxPool2d(( 1, shape[3] ))
         y_max = maxImg( y ).detach().numpy()[0][0].squeeze()  # >>> one-dimensional numpy array of max values in each row of filtered photo
         Y_mean = np.mean( y_max )
         upper_limit = np.where( y_max > Y_mean )[0][0]
