bb59e48ac61053fb6965bd02a6db09bf0d5c975e
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-07-19 15:48:57 +0300

    Draft: number detector

--- app.py
+++ app.py
@@ -16,20 +16,35 @@ from os import system
 ## where are we loking for 
 tensor_cactus      = loadImageAsTensor( "images/cacti/mid-3.jpg" )
 tensor_t_rex       = loadImageAsTensor( "images/t-rex/t-rex-top-1.jpg" )
-#tensor_distance   = loadImageAsTensor( "images/distance/near.jpg" )
-#tensor_distance    = loadImageAsTensor( "images/distance/center-1.jpg" )
-tensor_distance   = loadImageAsTensor( "images/distance/none-N.jpg" )
+tensor_distance    = loadImageAsTensor( "images/distance/none-N.jpg" )
 tensor_go          = loadImageAsTensor( "images/GO/go_none.jpg" )
-#tensor_go          = loadImageAsTensor( "images/GO/go-1.jpg" )
-#print( tensor_go )
-#print( tensor_go.shape )
-#tensor_go          = loadImageAsTensor( "images/GO/go_none.jpg" )
-#viewImg( tensor_distance )
+### number_tensors
+tensor_0          = loadImageAsTensor( "images/numbers/0.jpg" )
+tensor_1          = loadImageAsTensor( "images/numbers/1.jpg" )
+tensor_2          = loadImageAsTensor( "images/numbers/2.jpg" )
+tensor_3          = loadImageAsTensor( "images/numbers/3.jpg" )
+tensor_4          = loadImageAsTensor( "images/numbers/4.jpg" )
+tensor_5          = loadImageAsTensor( "images/numbers/5.jpg" )
+tensor_6          = loadImageAsTensor( "images/numbers/6.jpg" )
+tensor_7          = loadImageAsTensor( "images/numbers/7.jpg" )
+tensor_8          = loadImageAsTensor( "images/numbers/8.jpg" )
+tensor_9          = loadImageAsTensor( "images/numbers/9.jpg" )
 
 ## what are we loking for
 weights_t_rex  = loadImageAsTensor( "images/masks/t-rex-weights.jpg" )
 weights_cactus = loadImageAsTensor( "images/masks/cactus-weights.jpg" )
 weights_go     = loadImageAsTensor( "images/masks/go.jpg" )
+### number_weights
+weights_0     = loadImageAsTensor( "images/masks/numbers/0.jpg" )
+weights_1     = loadImageAsTensor( "images/masks/numbers/1.jpg" )
+weights_2     = loadImageAsTensor( "images/masks/numbers/2.jpg" )
+weights_3     = loadImageAsTensor( "images/masks/numbers/3.jpg" )
+weights_4     = loadImageAsTensor( "images/masks/numbers/4.jpg" )
+weights_5     = loadImageAsTensor( "images/masks/numbers/5.jpg" )
+weights_6     = loadImageAsTensor( "images/masks/numbers/6.jpg" )
+weights_7     = loadImageAsTensor( "images/masks/numbers/7.jpg" )
+weights_8     = loadImageAsTensor( "images/masks/numbers/8.jpg" )
+weights_9     = loadImageAsTensor( "images/masks/numbers/9.jpg" )
 
 
 #class initialization 
@@ -68,14 +83,14 @@ else:
     print( f"item >>> {item}" )
     
 
-## detect status game
+## detect numbers
 getConvTensor = nn.Conv2d( 1, 1, ( 13, 22 ))
-getConvTensor.weight = nn.Parameter( weights_go )
-conv_result = getConvTensor( tensor_go ).detach()
+getConvTensor.weight = nn.Parameter( weights_0 )
+conv_result = getConvTensor( tensor_0 ).detach()
 sq = torch.squeeze( conv_result )
 item = sq.item()/100
 if item > 1.5:
-    print( "GAME OVER!!!" )
+    print( "number was detected correct" )
 else:
     print( f"item >>> {item}" )
 
