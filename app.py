79d433465eeaa2231a80dd7c57f28de498b11c65
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-09-19 16:21:42 +0300

    Finished digit detector

--- app.py
+++ app.py
@@ -71,13 +71,19 @@ else:
    ### Caution! Number ONE is 8 pixels wide, another numbers has 9 pixels wide
 tensor_score_np_array = tensor_score.detach().to('cpu').numpy().squeeze()       #transform score tensor to numpy array 
 
-digit = tensor_score_np_array[ :, 0:11 ]                                        #isolate first difit from score tensor
-x_digit = torch.from_numpy( digit ).unsqueeze(0).unsqueeze(0)                   #transform numpy digit to 4-dimention tensor
-getConvTensor = nn.Conv2d( 1, 1, ( 13, 11 ))
-getConvTensor.weight = nn.Parameter( weights_list[ 9 ])                     
-conv_result = getConvTensor( x_digit ).detach().squeeze()          
-print( f"conv_result >>> {conv_result}" )
-
+block = 0
+for i in range( 5 ):
+    
+    digit = tensor_score_np_array[ :, block:block + 11 ]                                        #isolate first difit from score tensor
+    x_digit = torch.from_numpy( digit ).unsqueeze(0).unsqueeze(0)                   #transform numpy digit to 4-dimention tensor
+    getConvTensor = nn.Conv2d( 1, 1, ( 13, 11 ))
+    
+    for k in range( 10 ):
+        getConvTensor.weight = nn.Parameter( weights_list[ k ])                     
+        conv_result = getConvTensor( x_digit ).detach().squeeze()          
+        if conv_result > 10:
+            print( f"detected digit is >>> {k}" )
+    block += 11
 
 
 
--- data.py
+++ data.py
@@ -9,7 +9,7 @@ tensor_cactus      = loadImageAsTensor( "images/cacti/mid-3.jpg" )
 tensor_t_rex       = loadImageAsTensor( "images/t-rex/t-rex-top-1.jpg" )
 tensor_distance    = loadImageAsTensor( "images/distance/none-N.jpg" )
 tensor_go          = loadImageAsTensor( "images/GO/go_none.jpg" )
-tensor_score       = loadImageAsTensor( "images/scores/00362.jpg" )
+tensor_score       = loadImageAsTensor( "images/scores/00042.jpg" )
 
 
 ### number_tensors
