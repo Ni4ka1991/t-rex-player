246ae312fe597d61fdce5a61fb038c8a24f2a40e
astahova veronica <veronika@Veronika-notebook.localdomain>
2022-12-27 16:08:27 +0200

    │AttributeError: 'NoneType' object has no attribute 'set_data'

--- app.py
+++ app.py
@@ -13,18 +13,41 @@ import numpy as np
 system( "clear" )
 
 img          = None
+fig          = None
 frame_tensor = None
 animation    = None
+plot         = None
+
 browser, canvas = connectToClient() 
 frame_tensor    = getCanvasTensor( browser, canvas )
 zoneA, zoneB, zoneC, zoneD, zoneE = splitTensorToZones( frame_tensor )
 
+data_x = [ i for i in range(50) ]
+data_y = [ i for i in range(50) ]
+
+data_x2 = [ 4, 5, 6 ]
+data_y2 = [ 4, 5, 6 ]
 def initPlot(): 
     global img
+    global fig
     global animation
     global frame_tensor
     global zoneA, zoneB, zoneC, zoneD, zoneE
-    img = plt.imshow( catZonesToTensor( zoneA, zoneB, zoneC, zoneD, zoneE ))
+    global data_x, data_y
+    
+    fig = plt.figure(figsize = ( 5, 5 ))
+    rows = 3
+    cols = 1
+
+    fig.add_subplot( rows, cols, 1 )
+    img = plt.imshow( catZonesToTensor( zoneA, zoneB, zoneC, zoneD, zoneE )) #var "img" save the link to the image
+
+    fig.add_subplot( rows, cols, 2 )
+    plot, = plt.plot( data_x, data_y )                                       #var "plot" save 2 links
+
+    fig.add_subplot( rows, cols, 3 )
+    plot, = plt.plot( data_x2, data_y2 )                                       #var "plot" save 2 links
+
     animation = FuncAnimation( plt.gcf(), update, frames=10, interval = 1 )
     
     plt.show( ) 
@@ -38,6 +61,7 @@ def update( frame_i ):
     zoneA, zoneB, zoneC, zoneD, zoneE = splitTensorToZones( frame_tensor )
     
     img.set_data( catZonesToTensor( zoneA, zoneB, zoneC, zoneD, zoneE ))
+    plot.set_data( data_x, data_y )
     return img, "figure 1"
     
 
