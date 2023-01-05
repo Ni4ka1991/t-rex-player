693f18707cb625e2bbc0f3a183aa72953d0d1afa
astahova veronica <veronika@Veronika-notebook.localdomain>
2023-01-05 16:03:13 +0200

    T-rex detector test

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ from PIL import Image, ImageOps
 #SEARCING DATA
 ## where are we loking for 
 tensor_cactus      = loadImageAsTensor( "images/cacti/mid-3.jpg" )
-tensor_t_rex       = loadImageAsTensor( "images/t-rex/t-rex-top-1.jpg" )
+tensor_t_rex       = loadImageAsTensor( "images/t-rex/t-rex-bottom-1.jpg" )
 tensor_distance    = loadImageAsTensor( "images/distance/none-N.jpg" )
 tensor_go          = loadImageAsTensor( "images/GO/go_none.jpg" )
 tensor_score       = loadImageAsTensor( "images/scores/00042.jpg" )
---
+++ test.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# module detect
+
+
+#TORCH
+import torch
+from torch import nn
+from torchvision.transforms.functional import rgb_to_grayscale, invert, normalize
+from torchvision import transforms
+from torchvision.io import read_image
+
+#NUMPY
+import time
+import random
+import numpy as np
+from helper_func import *
+from data import *
+from os import system
+
+class detectSomething():
+
+    def getMaxpooledImg( self, arr_dim, weights, tensor, maxpool_axis ):
+        
+        filteredImg = nn.Conv2d( 1, 1, ( arr_dim, arr_dim ))
+        filteredImg.weight = nn.Parameter( weights )
+        y_filtered = filteredImg( tensor )
+        shape = y_filtered.shape
+#        viewImg( y_filtered )
+
+        ###select maxpooling axis
+        if maxpool_axis == "v":
+            pass
+        elif maxpool_axis == "h":
+            y_filtered = y_filtered.permute( 0, 1, 3, 2 )
+            shape = y_filtered.shape
+
+
+        maxpoolingImg = nn.MaxPool2d(( 1, shape[3] ))
+        y_max = maxpoolingImg( y_filtered )        
+                
+        return y_max
+
+
+                
+    def getCoordinates( self, y ):
+        y_np_arr = getDataArray( y )
+        y_mean = np.mean( y_np_arr )
+        y_max = np.max( y_np_arr )
+#        viewData( y_np_arr, y_mean, "data visualization" )
+#        print( np.where( y_np_arr > y_mean ))
+        detect_limit = ( y_mean + y_max ) / 2
+        upper_limit = np.where( y_np_arr > detect_limit )[0][0]
+#        return y_np_arr        
+        return upper_limit
+
+
+#class initialization 
+ds = detectSomething()
+
+
+#DETECTING
+
+##detect t-rex
+def detectPlayerPosition( tensor, weights ):
+    arr_dim = weights_t_rex.shape[3]
+    t_rex_max = ds.getMaxpooledImg( arr_dim, weights_t_rex, tensor_t_rex, "v" )
+    t_rex_pos = torch.argmax( t_rex_max )
+#    return t_rex_pos
+    print( f"t_rex_position >>> {t_rex_pos}" )
+
+
+detectPlayerPosition( tensor_t_rex, weights_t_rex )
