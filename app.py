69b3b6df41a22d2aa076d0d4abf2bab44b5ee40d
Ni4ka1991 <astahova_nica@mail.ru>
2023-02-03 20:14:53 +0200

    add distance def

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 
+
 from enviroment import Enviroment
 
 env = Enviroment()
 
 env.render()
+env.distance( env.player_coords, env.target_coords )
+
+env.player_coords = [ 3, 3 ]
+env.distance( env.player_coords, env.target_coords )
--- enviroment.py
+++ enviroment.py
@@ -1,10 +1,11 @@
 #module Enviroment
 
 from os import system
+import math
 
 class Enviroment:
 
-    def __init__():
+    def __init__( self ):
         self.reset()
 
     def reset( self ):
@@ -15,14 +16,27 @@ class Enviroment:
 
     def render( self ):
         system( "clear" )
+        print( "_"*11 )
         for y in range(5):
+            print( "|", end = '' )
             for x in range(5):
                 if [ x, y ] == self.player_coords:
-                    print("P", end = " " )
+                    print("P", end = "|" )
                 elif [ x, y ] == self.target_coords:
-                    print("T", end = " " )
+                    print("T", end = "|" )
                 else:
-                    print(" ", end = " " )
+                    print(" ", end = "|" )
             print()
-        print( "-"*10 )
+            print( "_" * 11 )
         print( f"score = {self.player_score}" )
+
+
+    def distance( self, point1, point2 ):
+        dist = math.sqrt(( point1[0] - point2[0])**2 + ( point1[1] - point2[1])**2 ) 
+        print( f"distance:", dist )
+        return dist
+#        return math.sqrt(( point1[0] - point2[0])**2 + ( point1[1] - point2[1])**2 ) 
+
+
+    def step( self, action = None ):
+        self.player_coords[0] += 1
